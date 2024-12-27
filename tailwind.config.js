e139b5d2d8051f877d0e9d62bd962b14c0ec1918
Reidmen <r.rethmawn@gmail.com>
2024-12-27 09:04:11 +0000

    animated ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,45 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'record-pulse': 'record-pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'spin-slow': 'spin 2s linear infinite',
+        'bounce-slow': 'bounce 2s infinite',
+      },
+      keyframes: {
+        'record-pulse': {
+          '0%': {
+            transform: 'translate(-50%, -50%) scale(1)',
+            opacity: 1,
+          },
+          '100%': {
+            transform: 'translate(100%, -50%) scale(2)',
+            opacity: 0,
+          },
+        },
+        'pulse': {
+          '0%, 100%': {
+            opacity: 1,
+          },
+          '50%': {
+            opacity: .5,
+          },
+        },
+      },
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+      },
+      scale: {
+        '102': '1.02',
+      },
+      boxShadow: {
+        'button': '0 2px 4px rgba(0, 0, 0, 0.05), 0 1px 2px rgba(0, 0, 0, 0.1)',
+        'button-hover': '0 4px 6px rgba(0, 0, 0, 0.05), 0 2px 4px rgba(0, 0, 0, 0.1)',
+      },
+    },
   },
   plugins: [],
 }
